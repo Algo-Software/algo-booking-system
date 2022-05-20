57e2e433786fe3026747c4f6c63f56cf56000cdc
Ero Aguilar <eroaguilar7@gmail.com>
2022-05-20 10:24:46 +0800

    Found a way to show static page through routes.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', 'login');
+Route::redirect('/', 'public/homepage.html');
 
 Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
